b2512b26d5e81b3aa39a5b3ca4e41b2ffeca6b3c
r4zc4llthe3rd@hotmail.com <raz@Esko-ReBase.>
2024-10-17 22:17:23 +0200

    layer hole correction

--- config/layers.h
+++ config/layers.h
@@ -8,47 +8,47 @@
 #define RAISE 2
 #define FUN 3
 #define NAVI 4
-#define ADJUST 6
+#define ADJUST 5
 //aenhym layers
 // Alphas and OS overlays
-#define DEF 7
-#define WIN 8
-#define MAC 9
+#define DEF 6
+#define WIN 7
+#define MAC 8
 
 
 // Symbols and Alternate Alphas
-#define SYM 10
-#define WIN_SYM 11
-#define MAC_SYM 12
+#define SYM 9
+#define WIN_SYM 10
+#define MAC_SYM 11
 
 COND_LAYER(WIN SYM, WIN_SYM)
 COND_LAYER(MAC SYM, MAC_SYM)
 
 
 // Numbers and Function keys
-#define NUM 13
-#define WIN_NUM 14
-#define MAC_NUM 15
+#define NUM 12
+#define WIN_NUM 13
+#define MAC_NUM 14
 
 COND_LAYER(WIN NUM, WIN_NUM)
 COND_LAYER(MAC NUM, MAC_NUM)
 
 
 // Navigation & Media
-#define NAV 16
-#define WIN_NAV 17
-#define MAC_NAV 18
+#define NAV 15
+#define WIN_NAV 16
+#define MAC_NAV 17
 
 COND_LAYER(WIN NAV, WIN_NAV)
 COND_LAYER(MAC NAV, MAC_NAV)
 
 
 // Bluetooth & Keyboard
-#define ADM 19
+#define ADM 18
 
 COND_LAYER(NAV SYM, ADM)
 
 
 // Gaming
-#define GAM 20
-#define GAM_ARC 21
+#define GAM 19
+#define GAM_ARC 20
